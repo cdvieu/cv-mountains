7f3b7a3dfb8539f0f89fbd0aecece0bbaac8b030
Chris Vieu <cdvieu@gmail.com>
2023-06-06 20:57:08 -0400

    update to table

--- src/Table.js
+++ src/Table.js
@@ -14,7 +14,7 @@ function createData(name, date, height, carbs, protein) {
 
 const rows = [
   createData('Mount Washington', '7/5/2014', "5000'", 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Mount Osceola', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
